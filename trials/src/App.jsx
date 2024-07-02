19bc782b5a7892ba18cea8f25804acbc9bf5c4b0
ZhRaF <fatimarahmoune84@gmail.com>
2024-07-02 11:58:30 +0200

    fetching with useReducer

--- trials/src/App.jsx
+++ trials/src/App.jsx
@@ -1,14 +1,32 @@
-import { ContextProvider } from './context/ContextProvider';
-import Component from './Components/Component';
-
+import { useReducer,useEffect } from 'react'
+import axios from 'axios'
+import { reducer } from './reducer/reducer'
 import './App.css'
+import { initialState } from './reducer/reducer'
 
 function App() {
+  const [state,dispatch]=useReducer(reducer,initialState)
+
+  useEffect(() => {
+
+    axios.get('https://jsonplaceholder.typicode.com/todos/1')
+    .then(
+      (resp)=>{
+        dispatch({type:'SUCCESS',payload:resp.data})})
+    .catch(()=>{
+      dispatch({type:'FAILURE'})
+    })
+  }
+  
+  , [])
+  
   return (
     <>
-    <ContextProvider>
-        <Component/>
-    </ContextProvider>
+    <div>
+      {state.loading? '...loading' :'' }
+      {state.error? 'sorry something went wrong' :  state.post.title} 
+    </div>
+
 
     </>
   )
--- trials/src/reducer/reducer.js
+++ trials/src/reducer/reducer.js
@@ -1,10 +1,26 @@
-export const initialState = {arr:['first']}
+export const initialState = {
+    loading : true,
+    error:false,
+    post:{}
+}
 export const reducer = (state,action)=>{
     switch (action.type){
-    case 'ADD':
-    return {arr:[...state.arr,action.payload]} 
+    case 'SUCCESS':
+    return {
+        loading : false,
+        error:false,
+        post:action.payload
+    } 
+    case 'FAILURE':
+    return {
+        loading : false,
+        error:true,
+        post:{}
+        
+    }
     default:
         return state
+ 
 
 
 }
